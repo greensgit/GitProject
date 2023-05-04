90dff5d7134232b08cd48343ea081e158c28c7c6
greensgit <rajgreensomr@gmail.com>
2023-05-04 10:37:34 +0530

    new changes by Ajith

--- src/org/tcs/test/ClassDetails.java
+++ src/org/tcs/test/ClassDetails.java
@@ -5,6 +5,7 @@ public class ClassDetails {
 	public static void main(String[] args) {
 		
 		System.out.println("Class is GIT");
+		System.out.println("branch name is Ajith - workinng on Add to kart");
 	}
 
 }
